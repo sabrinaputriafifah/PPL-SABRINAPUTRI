6998c431facaa970f89ab8402c0287aa1ad287a1
2313030004 <sabrinaputriafifah@gmail.com>
2025-10-08 20:02:57 +0700

    mengubah 25 menjadi 26

--- unit-test/test_age.php
+++ unit-test/test_age.php
@@ -4,7 +4,7 @@ require_once "Validator.php";
 
 // Test Case 1: umur valid
 try {
-    $result = validateAge(25);
+    $result = validateAge(26);
     echo "PASS: Umur 25 diterima\n";
 } catch (Exception $e) {
     echo "FAIL: Umur 25 tidak diterima. Error: " . $e->getMessage() . "\n";
